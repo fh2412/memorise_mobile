88c5d86486945fcdd739e23d9ff02327b9ac9c47
Flo <florianhofer024@gmail.com>
2026-02-21 08:11:38 +0100

    progress issue #3: added users screen

---
+++ lib/ui/user/view_models/user_screen_view_model.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../../../data/repositories/user_repository.dart';
+import '../../../domain/models/user_model.dart';
+
+class UserScreenViewModel extends ChangeNotifier {
+  final UserRepository _userRepository;
+
+  // State variables
+  MemoriseUser? user;
+  bool isLoading = false;
+  String? error;
+
+  UserScreenViewModel(this._userRepository);
+
+  // Initial fetch called when the app starts/home loads
+  Future<void> fetchUserData() async {
+    final firebaseUid = FirebaseAuth.instance.currentUser?.uid;
+    if (firebaseUid == null) return;
+
+    isLoading = true;
+    error = null;
+    notifyListeners();
+
+    try {
+      user = await _userRepository.getUser(firebaseUid);
+    } catch (e) {
+      error = "Could not fetch user data: $e $user";
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+}
---
+++ lib/ui/user/views/user_screen_view.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:memorise_mobile/ui/user/views/friend_add_row_view.dart';
+import 'package:memorise_mobile/ui/user/views/friend_list_view.dart';
+import 'package:memorise_mobile/ui/user/views/user_card_view.dart';
+import 'package:provider/provider.dart';
+import '../view_models/user_screen_view_model.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("User Page"),
+        actions: [
+          IconButton(icon: const Icon(Icons.settings), onPressed: () => ''),
+        ],
+      ),
+      body: Consumer<UserScreenViewModel>(
+        builder: (context, vm, child) {
+          if (vm.isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (vm.error != null) {
+            return Center(
+              child: Text(vm.error!, style: const TextStyle(color: Colors.red)),
+            );
+          }
+
+          final user = vm.user;
+          if (user == null) return const Center(child: Text("No user found"));
+
+          return Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                const UserCard(),
+                const SizedBox(height: 25),
+                const FriendAddRow(),
+                const SizedBox(height: 25),
+                const Expanded(child: FriendList()),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
